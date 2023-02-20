09013846da7b7b58e8f718ad15747d758a9a37b7
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-02-20 12:58:51 -0300

    ✨ feat: Create initial dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header';
+
+export default function Dashboard() {
+  return (
+    <h1>
+      <Header />
+      <div>
+        <h1>asdasd</h1>
+      </div>
+    </h1>
+  );
+}
