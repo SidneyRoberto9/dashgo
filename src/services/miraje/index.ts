87c3b896cd4f3f8cd310bfafb1fd09a8e768c06f
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-03-09 15:55:58 -0300

    📦 refactor: update size miraje js

--- src/services/miraje/index.ts
+++ src/services/miraje/index.ts
@@ -1,5 +1,11 @@
 import faker from 'faker';
-import { createServer, Factory, Model } from 'miragejs';
+import {
+  ActiveModelSerializer,
+  createServer,
+  Factory,
+  Model,
+  Response,
+} from 'miragejs';
 
 type User = {
   name: string;
@@ -9,35 +15,53 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
     models: {
       user: Model.extend<Partial<User>>({}),
     },
-
     factories: {
       user: Factory.extend({
-        name(i: number) {
-          return `User ${i + 1}`;
+        name() {
+          return faker.name.findName();
         },
         email() {
           return faker.internet.email().toLowerCase();
         },
         createdAt() {
-          return faker.date.recent(10);
+          return faker.date.recent(20);
         },
       }),
     },
-
     seeds(server) {
-      server.createList('user', 10);
+      server.createList('user', 20);
     },
-
     routes() {
       this.namespace = 'api';
       this.timing = 750;
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+        const total = schema.all('user').length;
 
-      this.get('/users');
+        const pageStar = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStar + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStar,
+          pageEnd,
+        );
+
+        return new Response(
+          200,
+          {
+            'x-total-count': String(total),
+          },
+          { users },
+        );
+      });
+      this.get('/users/:id');
       this.post('/users');
-
       this.namespace = '';
       this.passthrough();
     },
